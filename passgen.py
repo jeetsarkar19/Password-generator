00cb2fed9beae819627f439f070fdd4e8e6a69fe
Jeet Sarkar <75967415+jeetsarkar19@users.noreply.github.com>
2020-12-16 01:27:39 +0530

    Pass_gen_GUI.py
    Adding a GUI using Tkinter, put dimensions aren't appropiately specified

--- passgen.py
+++ passgen.py
@@ -1,7 +1,9 @@
-# This is the password generator using random letters
 # This is a small attempt on building a password generator and tweaking it into a gui type application
 import random as rn
-def pass_gen(n):
+import tkinter as tk
+blank=tk.Entry()
+def pass_gen():
+    n = rn.randint(10,20)
     pas=''
     space1 =[]
     for k in range(33, 125):
@@ -12,7 +14,7 @@ def pass_gen(n):
         pas = pas+m
     pass_check(pas, n)
     return (pas)
-
+# Creating the password checker, that is, whether it manages to create a password which can be accepted by the websites 
 def pass_check(pas,n):
     c1=c2=c3=c4=0
     for j in pas:
@@ -26,11 +28,25 @@ def pass_check(pas,n):
         else:
             c4+=1
     if (c1>0 and c2>0 and c3>0 and c4>0):
-        print("The password is",pas)
+        blank.insert(0,pas)
+        
     else:
-        pass_gen(n)
+        pass_gen()
+def del_text():
+    blank.delete(0,tk.END)
+    return 0
+#Turning the text based code into a GUI application 
 
-------------# MAIN---------------------------------
-num = int(input("Enter the number of characters : "))
-pass_gen(num)
+win = tk.Tk()
+
+frame1=tk.Frame()
+frame1.grid(row=0,column=2)
+label=tk.Label(frame1,text="Welcome to the Password Generator")
+btn1=tk.Button(text="click me", command=pass_gen)  
+btn2=tk.Button(text="Delete", command=del_text)      
+label.pack()
+btn1.grid(row=2,column=2)
+btn2.grid(row=2,column=3)
+blank.grid(row=3,column=2)
+win.mainloop() 
 
