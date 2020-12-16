87b28bcd021f0268d736dfc9e749b36bb4b4d6d6
Jeet Sarkar <75967415+jeetsarkar19@users.noreply.github.com>
2020-12-16 12:08:35 +0530

    Update and rename passgen.py to pass_gen2.py

--- pass_gen2.py
+++ pass_gen2.py
@@ -1,9 +1,6 @@
 # A small GUI application of password generator
 import random as rn
-import tkinter as tk
-win=tk.Tk()
-win.title("Password Generator")
-blank=tk.Entry(master=win)
+
 def pass_gen():
     n = rn.randint(10,20)
     pas=''
@@ -14,8 +11,9 @@ def pass_gen():
     list2 = rn.choices(space1, k=n)
     for m in list2:
         pas = pas+m
+    return (pas)    
     pass_check(pas, n)
-    return (pas)
+    
 # Creating the password checker, that is, whether it manages to create a password which can be accepted by the websites 
 def pass_check(pas,n):
     c1=c2=c3=c4=0
@@ -30,25 +28,15 @@ def pass_check(pas,n):
         else:
             c4+=1
     if (c1>0 and c2>0 and c3>0 and c4>0):
-        blank.insert(0,pas)
+        return(pas)
     else:
         pass_gen()
-def del_text():
-    blank.delete(0,tk.END)
-    return 0
-def quit_gui():
-    win.destroy()
-    return 0
+#-------------------------------------------------MAIN-------------------------------------------------------#
+num=rn.randint(10,20)
+p=pass_gen(num)
+print(f"The given password is {p}")
 
-#Turning the text based code into a GUI application 
 
-label=tk.Label(master=win,text="Welcome to the Password Generator")
-btn1=tk.Button(master=win,text="click me", command=pass_gen)  
-btn2=tk.Button(master=win,text="Delete", command=del_text)  
-btn3=tk.Button(master=win,text="Quit", command=quit_gui)   
-label.grid(row=0,column=1)
-btn1.grid(row=2,column=0)
-btn2.grid(row=2,column=1)
-btn3.grid(row=2,column=2)
-blank.grid(row=1,column=1)
-win.mainloop()
+
+
+
